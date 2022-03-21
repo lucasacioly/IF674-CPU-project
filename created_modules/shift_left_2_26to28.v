c20824aace3657907a38f7a79abbddad030d2b9b
Jose Lucas Acioly <lucasacioly@outlook.com>
2022-03-21 17:04:55 -0300

    modulos de shift adicionados

--- created_modules/shift_left_2_26to28.v
+++ created_modules/shift_left_2_26to28.v
@@ -1,6 +1,6 @@
 module shift_left_2_26to28(
-    input wire [15:0] data_in,
-    output wire [31:0] data_out
+    input wire [25:0] data_in,
+    output wire [27:0] data_out
 );
 
     assign data_out = {data_in, {2{1'b0}}};
