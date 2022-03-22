a33349467d741a245c83cc799c26e19a6892f041
ksbs22 <ksbs@cin.ufpe.br>
2022-03-22 18:07:58 -0300

    Atualizaçao: erro nos tamanhos das entradas dos mux

--- created_modules/mux_3x1.v
+++ created_modules/mux_3x1.v
@@ -1,13 +1,13 @@
 module mux_3x1 (
-    input wire [32:0] Data_0,
-    input wire [32:0] Data_1,
-    input wire [32:0] Data_2,
+    input wire [31:0] Data_0,
+    input wire [31:0] Data_1,
+    input wire [31:0] Data_2,
 
     input wire [1:0] Selector,
-    output wire [32:0] Data_out
+    output wire [31:0] Data_out
 );
 
-wire [32:0] A1;
+wire [31:0] A1;
 assign A1 = (Selector[0]) ? Data_1 : Data_0;
 
 assign Data_out = (Selector[1]) ? Data_2 : A1;
--- created_modules/mux_3x1_5.v
+++ created_modules/mux_3x1_5.v
@@ -1,13 +1,13 @@
-module mux_3x1_5 (
-    input wire [5:0] Data_0,
-    input wire [5:0] Data_1,
-    input wire [5:0] Data_2,
+module mux_3x1_4 (
+    input wire [4:0] Data_0,
+    input wire [4:0] Data_1,
+    input wire [4:0] Data_2,
 
     input wire [1:0] Selector,
-    output wire [5:0] Data_out
+    output wire [4:0] Data_out
 );
 
-wire [5:0] A1;
+wire [4:0] A1;
 assign A1 = (Selector[0]) ? Data_1 : Data_0;
 
 assign Data_out = (Selector[1]) ? Data_2 : A1;
