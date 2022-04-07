51221a216d5ed2cb14fd792b934a4e33e42f4555
Icaro-Nunes <icarolimanunes@gmail.com>
2022-04-07 19:11:26 -0300

    Testado mfhi_mflo - FUNCIONOU

--- given_modules/Memoria.vhd
+++ given_modules/Memoria.vhd
@@ -47,7 +47,7 @@
 package ram_constants is
 	constant DATA_WIDTH : INTEGER := 8;
 	constant ADDR_WIDTH : INTEGER := 8;
-	constant INIT_FILE  : STRING  := "addi_srav.mif";
+	constant INIT_FILE  : STRING  := "mfhi_mflo.mif";
 end ram_constants;
 
 --*************************************************************************
