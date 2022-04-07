16cec5732673eb9ddef609e757850a8f91e139b1
Icaro-Nunes <icarolimanunes@gmail.com>
2022-04-07 19:25:46 -0300

    NÃO FUNCIONOU BNE - Testado bne_break

--- given_modules/Memoria.vhd
+++ given_modules/Memoria.vhd
@@ -47,7 +47,7 @@
 package ram_constants is
 	constant DATA_WIDTH : INTEGER := 8;
 	constant ADDR_WIDTH : INTEGER := 8;
-	constant INIT_FILE  : STRING  := "mfhi_mflo.mif";
+	constant INIT_FILE  : STRING  := "bne_break.mif";
 end ram_constants;
 
 --*************************************************************************
