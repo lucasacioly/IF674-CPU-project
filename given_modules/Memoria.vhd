7b1e194b3c2cc23f020b00f147da3d7fd6646a0a
Icaro-Nunes <icarolimanunes@gmail.com>
2022-04-07 20:07:33 -0300

    Testado ble - FUNCIONOU

--- given_modules/Memoria.vhd
+++ given_modules/Memoria.vhd
@@ -47,7 +47,7 @@
 package ram_constants is
 	constant DATA_WIDTH : INTEGER := 8;
 	constant ADDR_WIDTH : INTEGER := 8;
-	constant INIT_FILE  : STRING  := "bgt.mif";
+	constant INIT_FILE  : STRING  := "ble.mif";
 end ram_constants;
 
 --*************************************************************************
