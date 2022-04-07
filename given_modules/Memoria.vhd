f2344c66e33b03c47188db936943c635e8a02f65
Icaro-Nunes <icarolimanunes@gmail.com>
2022-04-07 12:41:23 -0300

    Testado ADDI_SLLV - FUNCIONOU

--- given_modules/Memoria.vhd
+++ given_modules/Memoria.vhd
@@ -47,7 +47,7 @@
 package ram_constants is
 	constant DATA_WIDTH : INTEGER := 8;
 	constant ADDR_WIDTH : INTEGER := 8;
-	constant INIT_FILE  : STRING  := "addi_sll.mif";
+	constant INIT_FILE  : STRING  := "addi_sllv.mif";
 end ram_constants;
 
 --*************************************************************************
