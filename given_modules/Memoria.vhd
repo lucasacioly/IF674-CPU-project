7f32498eae133854c5648db70409f146562d0807
Geovany Icaro Lima Nunes <giln@WINDOWS.CIN.UFPE.BR>
2022-04-08 18:52:52 -0300

    Testado addm - FUNCIONOU

--- given_modules/Memoria.vhd
+++ given_modules/Memoria.vhd
@@ -47,7 +47,7 @@
 package ram_constants is
 	constant DATA_WIDTH : INTEGER := 8;
 	constant ADDR_WIDTH : INTEGER := 8;
-	constant INIT_FILE  : STRING  := "lui.mif";
+	constant INIT_FILE  : STRING  := "addm.mif";
 end ram_constants;
 
 --*************************************************************************
